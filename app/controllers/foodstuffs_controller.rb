db5f60998c9f3adb9bf2eb6e55827a39f6ce1176
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-12-23 21:16:15 +0900

    foodstuffコントローラー編集

--- app/controllers/foodstuffs_controller.rb
+++ app/controllers/foodstuffs_controller.rb
@@ -1,2 +1,27 @@
 class FoodstuffsController < ApplicationController
+
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @foodstuff = Foodstuff.new
+  end
+
+  
+
+  def create
+    @foodstuff = Foodstuff.new(foodstuff_params)
+  
+    if @foodstuff.save
+    redirect_to new_recipe_text_path( @foodstuff.recipe_id)
+    else
+      @recipe = @foodstuff.recipe
+      @foodstuffs = @recipe.foodstuffs
+      render :new
+    end
+  end
+
+  private
+  def foodstuff_params
+    params.require(:foodstuff).permit(:material, :quantity).merge(recipe_id: params[:recipe_id])
+  end
+  
 end
