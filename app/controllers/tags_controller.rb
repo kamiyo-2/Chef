aa39bee353bd2e2bc927c2bc5bbd84aaaa479356
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-12-23 22:30:44 +0900

    tags.index作成

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,19 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.find(1)
+    @tagss = Tag.find(2)
+    @tagsss = Tag.find(3)
+    @tagssss = Tag.find(4)
+    @tagsssss = Tag.find(5)
+    @tagssssss = Tag.find(6)
+    @tagsssssss = Tag.find(7)
+    @tagssssssss = Tag.find(8)
+    @tagsssssssss = Tag.find(9)
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+    @recipe = Recipe.new
+    @recipes = @tag.recipes
+  end
 end
