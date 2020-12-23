63426f35cacae7ab6066bc43bac549e96cadd4d4
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-12-23 21:15:18 +0900

    recipeコントローラー編集

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,25 @@
 class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    
+    @recipe = Recipe.new(recipe_params)
+    @recipe.valid?
+    if @recipe.save
+      @recipe[:id]
+      redirect_to new_recipe_foodstuff_path(@recipe.id)
+    else
+      render :new
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :process, :details, :main_image, tag_ids: []).merge(user_id: current_user.id)
+  end
+
 end
+
