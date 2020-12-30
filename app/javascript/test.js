e0dda8d0463740f1f9fd295cbe1d981cadab10a3
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2020-12-31 03:56:04 +0900

    recipe修正

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function() {
     const ImageList = document.getElementById('image-list')
@@ -41,3 +42,46 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
     });
   });
 }
+=======
+$(function(){
+  function buildField(index) {  // 追加するフォームのｈｔｍｌを用意
+    const html = `<div class="js-file-group" data-index="${index}">
+                    <div class="tag-area__title">
+                      タグ
+                    </div>
+                    <div class="tag-area__form">
+                      <input type="text" name="post[tags_attributes][${index}][content]" id="post_tags_attributes_${index}_content">
+                      <span class="delete-form-btn">
+                        削除する
+                      </span>
+                    </div>
+                  </div>`;
+    return html;
+  }
+    $(".tag-area").on("click", ".delete-form-btn", function() { // 削除ボタンクリックでイベント発火
+      $(".add-form-btn").css("display","block"); // どの道フォームは一つ消えるので、追加ボタンを必ず表示させるようにしておく
+      const targetIndex = $(this).parent().parent().data("index") // クリックした箇所のインデックス番号を取得
+      const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`); // 編集用（クリックした箇所のチェックボックスを取得）
+      var lastIndex = $(".js-file-group:last").data("index"); // フォームの最後に使われているインデックス番号を取得
+      displayCount -= 1; // 表示されているフォーム数を一つカウントダウン
+      if (targetIndex < fileCount) { // 編集用（チェックボックスがある場合の処理）
+        $(this).parent().parent().css("display","none") // フォームを非表示にする
+        hiddenCheck.prop("checked", true); // チェックボックスにチェックを入れる
+      } else { // チェックボックスがない場合の処理
+        $(this).parent().parent().remove(); // フォームを削除する
+      }
+      // ↓はfileIndex（フォーム追加ように用意してある数字の配列）の処理
+      if (fileIndex.length >= 1) { // 数字が一つでも残っている場合
+        fileIndex.push(fileIndex[fileIndex.length - 1] + 1); // 配列の一番右側にある数字に１を足した数字を追加
+      } else {
+        fileIndex.push(lastIndex + 1); // フォームの最後の数字に1を足した数字を追加
+      }
+      // ↓はフォームがなくならないための処理
+      if (displayCount == 0) { // 見えてるフォームの数が0になったとき
+        $(".tag-area").append(buildField(fileIndex[0] - 1)); // fileIndexの一番左側にある数字から１引いた数字でフォームを作成
+        fileIndex.shift(); // fileIndexの一番小さい数字を取り除く
+        displayCount += 1; // 見えているフォームの数をカウントアップしておく
+      } 
+    })
+  })
+>>>>>>> Stashed changes
