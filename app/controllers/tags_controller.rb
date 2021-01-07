c87c799cf953092d56be31214e044572cf102f43
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2021-01-08 06:55:27 +0900

    tag.controller修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,14 +1,5 @@
 class TagsController < ApplicationController
   def index
-    @tags = Tag.find(1)
-    @tagss = Tag.find(2)
-    @tagsss = Tag.find(3)
-    @tagssss = Tag.find(4)
-    @tagsssss = Tag.find(5)
-    @tagssssss = Tag.find(6)
-    @tagsssssss = Tag.find(7)
-    @tagssssssss = Tag.find(8)
-    @tagsssssssss = Tag.find(9)
   end
 
   def show
