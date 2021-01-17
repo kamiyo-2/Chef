afc4b8f1904f237786085d0214d670641c162848
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2021-01-17 20:34:37 +0900

    foodstuff非同期実装

--- app/controllers/foodstuffs_controller.rb
+++ app/controllers/foodstuffs_controller.rb
@@ -8,10 +8,11 @@ class FoodstuffsController < ApplicationController
 
 
   def create
+    @recipe = Recipe.find params[:recipe_id]
     @foodstuff = Foodstuff.new(foodstuff_params)
-    @foodstuffs = Foodstuff.all
-    if @foodstuff.save
-    @foodstuff = Foodstuff.new
+    @foodstuffs = @recipe.foodstuffs.order(created_at: :desc)
+    if @foodstuff.save!
+      render :create
     end
   end
 
@@ -20,6 +21,15 @@ class FoodstuffsController < ApplicationController
     @foodstuffs = Foodstuff.where(recipe_id: @recipe.id)
     @foodstuff = Foodstuff.new
   end
+
+  def destroy
+    @foodstuff = Foodstuff.find(params[:id]) #⑤
+    if @foodstuff.destroy!
+      render :create
+    end
+  end
+
+  
 
   private
   def foodstuff_params
