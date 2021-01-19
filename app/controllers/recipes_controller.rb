8812486059e958e500aa1104389d3eab7d4d2cc9
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2021-01-19 18:20:19 +0900

    recipesコントローラー編集

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -30,6 +30,25 @@ class RecipesController < ApplicationController
     @recipephotos = @recipe.recipephotos.order(created_at: :desc)
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+    @foodstuff = Foodstuff.new
+    @foodstuffs = @recipe.foodstuffs
+    @recipephoto = Recipephoto.new
+    @recipephotos = @recipe.recipephotos.order(created_at: :desc)
+    if @recipe.user != current_user
+      render :edit
+    end
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    if recipe.update(recipe_params)
+      redirect_to recipe_path(recipe.id), method: :get
+    else
+      render :edit
+    end
+  end
 
   private
   def recipe_params
