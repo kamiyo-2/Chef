e4417408a20e86c8bdce088eb335da69919c266a
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2021-01-19 18:19:52 +0900

    recipes.edit 編集

--- app/controllers/recipephotos_controller.rb
+++ app/controllers/recipephotos_controller.rb
@@ -1,21 +1,5 @@
 class RecipephotosController < ApplicationController
 
-  def new
-    @recipe = Recipe.find(params[:recipe_id])
-    @foodstuffs = Foodstuff.where(recipe_id: @recipe.id)
-    @recipephoto = Recipephoto.new
-  end
-
-  def create
-    @recipephoto = Recipephoto.new(recipephoto_params)
-    if @recipephoto.save
-      redirect_to recipe_detail_recipe_path(@recipephoto.recipe)
-    else
-      @recipe = @recipephoto.recipe
-      @recipephotos = @recipe.recipephotos
-      render "recipes/show"
-    end
-  end
   def create
     @recipe = Recipe.find params[:recipe_id]
     @recipephoto = Recipephoto.new(recipephoto_params)
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,8 +22,6 @@ class RecipesController < ApplicationController
     @recipephotos = @recipe.recipephotos.order(created_at: :desc)
   end
 
-  
-
   def show
     @recipe = Recipe.find(params[:id])
     @foodstuff = Foodstuff.new
@@ -32,34 +30,11 @@ class RecipesController < ApplicationController
     @recipephotos = @recipe.recipephotos.order(created_at: :desc)
   end
 
-  def edit
-    @recipe = Recipe.find(params[:id])
-    @foodstuff = Foodstuff.new
-    @foodstuffs = @recipe.foodstuffs
-    @recipephoto = Recipephoto.new
-    @recipephotos = @recipe.recipephotos.order(created_at: :desc)
-    if @recipe.user != current_user
-      render :edit
-    end
-  end
-
-  def update
-    recipe = Recipe.find(params[:id])
-    if recipe.update(recipe_params)
-      redirect_to recipe_path(recipe.id), method: :get
-    else
-      render :edit
-    end
-  end
 
   private
   def recipe_params
     params.require(:recipe).permit(:title, :process, :details, :main_image, tag_ids: []).merge(user_id: current_user.id)
   end
-  # def recipe_params
-    
-  #   params.require(:recipe).permit(:title, :process, :details, :main_image, tag_ids: [], foodstuffs_attributes: [:material, :quantity]).merge(user_id: current_user.id)
-  # end
-
+ 
 end
 
