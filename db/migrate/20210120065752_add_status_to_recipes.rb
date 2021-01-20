fefa91667db5a4feda286867b611a3d2fe7f9188
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2021-01-21 01:35:38 +0900

    resipes.edit編集

--- db/migrate/20210120065752_add_status_to_recipes.rb
+++ db/migrate/20210120065752_add_status_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddStatusToRecipes < ActiveRecord::Migration[6.0]
   def change
-    add_column :recipes, :status, :integer, default: 1, null: false, limit: 1
+    add_column :recipes, :status, :integer, default: 0, null: false, limit: 1
   end
 end
