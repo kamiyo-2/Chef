f89b3eaea5747504fa6b869ee7e3964fd69dabd8
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2021-01-23 00:46:46 +0900

    admin,recipe作成

---
+++ app/controllers/admins/recipes_controller.rb
@@ -0,0 +1,40 @@
+class Admins::RecipesController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @foodstuff = Foodstuff.new
+    @foodstuffs = @recipe.foodstuffs
+    @recipephoto = Recipephoto.new
+    @recipephotos = @recipe.recipephotos.order(created_at: :desc)
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    @foodstuff = Foodstuff.new
+    @foodstuffs = @recipe.foodstuffs
+    @recipephoto = Recipephoto.new
+    @recipephotos = @recipe.recipephotos.order(created_at: :desc)
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to admins_recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :process, :details, :main_image, tag_ids: [])
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,44 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions'
-  }
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    passwords: 'users/passwords'
-  }
 
+  root to: 'tags#index'
+
+  devise_for :admins, skip: :all
+  devise_scope :admin do
+    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+    post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'    
+  end
+
+  namespace :admins do
+    resources :tags
+    resources :recipes
+    resources :users
+  end
+
+  devise_for :users, skip: :all
   devise_scope :user do
+    get 'users/sign_in' => 'users/sessions#new', as: 'new_user_session'
+    post 'users/sign_in' => 'users/sessions#create', as: 'user_session'
+    delete 'users/sign_out' => 'users/sessions#destroy', as: 'destroy_user_session'
+    get 'users/sign_up' => 'users/registrations#new', as: 'new_user_registration'
+    post 'users' => 'users/registrations#create', as: 'user_registration'
+    get 'users/password/new' => 'users/passwords#new', as: 'new_user_password'
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 
-  root to: 'tags#index'
+  # devise_for :users, controllers: {
+  #   registrations: 'users/registrations',
+  #   passwords: 'users/passwords'
+  # }
+
+  resources :users do
+    member do
+      get 'nonreleases'
+    end
+  end
+
+  resources :tags 
+
   resources :recipes do
     resources :foodstuffs
     resources :recipephotos
@@ -25,10 +52,5 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  resources :tags 
-  resources :users do
-  member do
-    get 'nonreleases'
-  end
-end
+
 end
