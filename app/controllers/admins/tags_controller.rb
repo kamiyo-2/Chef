3aad393310992554d0aa16ac5509ee0b1beeada8
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2021-01-24 01:11:50 +0900

    admin tag作成

--- app/controllers/admins/tags_controller.rb
+++ app/controllers/admins/tags_controller.rb
@@ -1,11 +1,48 @@
 class Admins::TagsController < ApplicationController
-  efore_action :authenticate_admin!
+  before_action :authenticate_admin!
   def index
-  tags = Tag.all
-  end
-  def show
-    @tag = Tag.find(params[:id])
-    @recipes = @tag.recipes
+  @tags = Tag.all
   end
 
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    @tag.valid?
+    if @tag.save
+      redirect_to admins_tags_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @tag = Tag.new
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      redirect_to admins_tags_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to admins_tags_path
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:name, :tag_image)
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,8 @@
 class Tag < ApplicationRecord
   has_many :recipe_tag_relations, dependent: :destroy
   has_many :recipes, through: :recipe_tag_relations
+  validates :name, :tag_image ,presence: true
+  has_one_attached :tag_image
 
+  
 end
