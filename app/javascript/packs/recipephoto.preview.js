9722cbe454fac65301e41e28c3aa19141e5c71a2
kamiyo-2 <71446248+kamiyo-2@users.noreply.github.com>
2021-01-27 16:55:20 +0900

    foodstuff css編集

--- app/javascript/packs/recipephoto.preview.js
+++ app/javascript/packs/recipephoto.preview.js
@@ -1,15 +1,16 @@
-if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+if (document.URL.match( /edit/ ) || document.URL.match( /detail/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
     const createImageHTML = (blob) => {
       const imageElement = document.createElement('div');
       const blobImage = document.createElement('img');
+      blobImage.setAttribute('class', 'user_image_index');
       blobImage.setAttribute('src', blob);
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
     };
     document.getElementById('recipephoto_sub_image').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');
+      const imageContent = document.querySelector('img.user_image_index');
       if (imageContent){
         imageContent.remove();
       }
